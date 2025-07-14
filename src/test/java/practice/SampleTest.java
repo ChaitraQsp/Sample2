c8a1cc7efdbd94653a8f1513f991ccd1a225fff0
Chaitra9632 <chaitra.sreenivas88@gmail.com>
2025-07-14 17:51:50 +0530

    Sample Commit

---
+++ src/test/java/practice/SampleTest.java
@@ -0,0 +1,19 @@
+package practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+
+public class SampleTest {
+
+    @Test
+    public void sampleTest(){
+
+        System.out.println("Hi");
+
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+    }
+}
